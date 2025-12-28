9884c927cbc1769b7f29b5e702d1cd307020c588
Pranay Bhardwaj <pranay29y@gmail.com>
2025-12-28 20:31:45 +0530

    Update best_time_to_buy_and_sell_stocks.cpp

--- Arrays/Arrays/best_time_to_buy_and_sell_stocks.cpp
+++ Arrays/Arrays/best_time_to_buy_and_sell_stocks.cpp
@@ -1,1 +1,19 @@
+// Problem: Best Time to Buy and Sell Stocks
+// Platform: LeetCode
+// Approach: Optimal
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int bestBuy = prices[0], maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++)
+        {
+            maxProfit = max(maxProfit,prices[i] - bestBuy);
+            bestBuy = min(bestBuy, prices[i]);
+        }
+        return maxProfit;
+    }
+};
