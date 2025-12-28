e089860f5cbbc86a6bf3cbc46af6fab62f613aab
Pranay Bhardwaj <pranay29y@gmail.com>
2025-12-28 21:10:03 +0530

    Added Find Peak Element solution using binary search

---
+++ Binary_Search/Binary_Search/find_peak_element.cpp
@@ -0,0 +1,20 @@
+// Problem: Find Peak Element
+// Platform: LeetCode
+// Approach: Optimal (Binary Search)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int low = 0, high = nums.size() - 1;
+        while(low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if(nums[mid] < nums[mid + 1]) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+};
