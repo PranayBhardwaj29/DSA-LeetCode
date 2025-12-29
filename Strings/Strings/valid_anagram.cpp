63a3712ed564d8b8ce395982648bdb03214ba87f
Pranay Bhardwaj <pranay29y@gmail.com>
2025-12-29 08:49:34 +0530

    Added Valid Anagram solution

---
+++ Strings/Strings/valid_anagram.cpp
@@ -0,0 +1,26 @@
+// Problem: Valid Anagram
+// Platform: LeetCode
+// Approach: Optimal (Counting Sort)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        int freq[26] = {0};
+        for(int i = 0; i < s.length(); i++)
+        {
+            freq[s[i] - 'a']++;
+        }
+        for(int i = 0; i < t.length(); i++)
+        {
+            freq[t[i] - 'a']--;
+        }
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(freq[s[i] - 'a'] != 0) return false;
+        }
+        return true;
+    }
+};
