814898b289d6534a7fd94d87183ce7cf8cb71e97
Pranay Bhardwaj <pranay29y@gmail.com>
2025-12-31 15:25:21 +0530

    Added Longest Substring Without Repeating Characters solution using sliding window approach

--- Strings/Strings/longest_substring.cpp
+++ Strings/Strings/longest_substring.cpp
@@ -3,3 +3,20 @@
 // Approach: Optimal (Sliding Window)
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLen = 0;
+        int l = 0;
+        unordered_map<char, int> mp;
+        for(int i = 0; i < s.length(); i++)
+        {
+            char ch = s[i];
+            if(mp.find(ch) != mp.end() && mp[ch] >= l) l = mp[ch] + 1;
+            mp[ch] = i;
+            maxLen = max(maxLen, i - l + 1);
+        }
+        return maxLen;
+    }
+};
